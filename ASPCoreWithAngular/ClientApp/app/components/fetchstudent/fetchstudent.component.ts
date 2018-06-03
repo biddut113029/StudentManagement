5eb1443a7b0c4b998fd3076699c616c0538751c4
biddut.hossain3029 <biddut.hossain113029@gmail.com>
2018-06-03 14:21:14 +0600

    okk

--- ASPCoreWithAngular/ClientApp/app/components/fetchstudent/fetchstudent.component.ts
+++ ASPCoreWithAngular/ClientApp/app/components/fetchstudent/fetchstudent.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { StudentService } from '../../services/stdservice.service'
 import { DataTableModule } from "angular2-datatable";
 import { NgModule } from '@angular/core';
+
 @Component({
     selector: 'fetchStudent',
     templateUrl: './fetchstudent.component.html'
@@ -19,16 +20,54 @@ import { NgModule } from '@angular/core';
  })
 export class FetchStudentComponent {
     public stdList: StudentData[];
+    public stdListTemp: StudentData[];
+    public pageList: PageData[];
 
+    public data = [];
+    public pseudoServer = [];
+    public filterQuery = "";
+    public rowsOnPage = 5;
+    public sortBy = "email";
+    public sortOrder = "asc";
+    public amountOfRows = 0;
+    public activePage = 1;
     constructor(public http: Http, private _router: Router, private _StudentService: StudentService) {
         this.getStudents();
     }
+    SetPage(page: any) {
 
-    getStudents() {
-        this._StudentService.getStudents().subscribe(
-            data => this.stdList = data
-        )
+        //this.activePage = page;
+        //this.getStudents();
     }
+    Next() {
+        
+        this.activePage = this.activePage + 1;
+        this.getStudents();
+    }
+    Previous()
+    {
+        if (this.activePage!=1)
+        this.activePage = this.activePage - 1;
+        this.getStudents();
+    }
+    getStudents() {
+        this._StudentService.getStudents().subscribe((data) => {
+            setTimeout(() => {
+
+                this.stdList = data;
+                this.stdListTemp = data;
+                this.load();
+                
+            }, 2000);
+
+        
+            });
+
+        
+    }
+
+
+
 
     delete(studentID) {
         var ans = confirm("Do you want to delete student with Id: " + studentID);
@@ -38,7 +77,108 @@ export class FetchStudentComponent {
             }, error => console.error(error)) 
         }
     }
+
+
+    public load() {
+        this.pageList = [];
+        this.amountOfRows = this.stdList.length;
+      
+
+         if (this.activePage == 1 && this.amountOfRows <= 5) {
+
+            this.pageList = [             
+                { id: this.activePage, status: 'blue' },
+            ];
+          
+            let start = (this.activePage - 1) * this.rowsOnPage;
+            this.stdList = this.stdList.slice(start, start + this.rowsOnPage);
+
+        }
+        else if (this.activePage == 1 && this.amountOfRows > 5 && this.amountOfRows <10 ) {
+
+            this.pageList = [
+                { id: this.activePage, status: 'blue' },
+                { id: this.activePage+1, status: 'yellow' },
+            ];
+        
+            let start = (this.activePage - 1) * this.rowsOnPage;
+            this.stdList = this.stdList.slice(start, start + this.rowsOnPage);
+
+        }
+        else if (this.activePage == 1 && this.amountOfRows > 10 ) {
+
+            this.pageList = [
+                { id: this.activePage, status: 'blue' },
+                { id: this.activePage+1, status: 'yellow' },
+                { id: this.activePage+2, status: 'yellow' },
+            ];
+            
+            let start = (this.activePage - 1) * this.rowsOnPage;
+            this.stdList = this.stdList.slice(start, start + this.rowsOnPage);
+
+        }
+       else if (this.activePage == 2 && this.amountOfRows > 5 && this.amountOfRows < 10 )
+        {
+            this.pageList = [
+                { id: this.activePage, status: 'blue' },
+                { id: this.activePage + 1, status: 'yellow' },
+                        
+            ];
+            
+            let start = (this.activePage - 1) * this.rowsOnPage;
+            this.stdList = this.stdList.slice(start, start + this.rowsOnPage);
+
+        }
+       else if (this.activePage == 2 && this.amountOfRows > 10 ) {
+            this.pageList = [
+                { id: this.activePage, status: 'blue' },
+                { id: this.activePage + 1, status: 'yellow' },            
+                { id: this.activePage + 2, status: 'yellow' },
+
+            ];
+            
+            let start = (this.activePage - 1) * this.rowsOnPage;
+            this.stdList = this.stdList.slice(start, start + this.rowsOnPage);
+
+        }
+       else if (this.activePage >= 3)
+        {
+            this.pageList = [
+                { id: this.activePage - 1, status: 'yellow' },
+                { id: this.activePage, status: 'blue' },
+                { id: this.activePage + 1, status: 'yellow' }
+            ];
+            
+            let start = (this.activePage - 1) * this.rowsOnPage;
+            this.stdList = this.stdList.slice(start, start + this.rowsOnPage);
+        }
+     
+    
+        
+        
+    }
+
+    public toInt(num: string) {
+        return +num;
+    }
+
+  
+
+    public onPageChange(event) {
+        this.rowsOnPage = event.rowsOnPage;
+        //this.activePage = event.activePage;
+        this.activePage = 1;
+        this.load();
+    }
+
+
+
+
+
 }
+
+
+
 
 interface StudentData {
     id: number;
@@ -52,4 +192,11 @@ interface StudentData {
     motherName: string;
     contactNo: string;
     email: string;
+}
+
+
+
+interface PageData {
+    id: number;
+    status: string;
 }
